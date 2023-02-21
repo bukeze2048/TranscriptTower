4d51fad57e578453dd84376ec00aea4160f9753c
Ezekiel Ogunkunle <bukeze2119@outlook.com>
2023-02-21 01:52:39 +0100

    created models, admin and views for students

--- student/views.py
+++ student/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student
+from .forms import StudentForm
 
-# Create your views here.
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/student_list.html', {'students': students})
+
+def student_detail(request, pk):
+    student = Student.objects.get(pk=pk)
+    return render(request, 'students/student_detail.html', {'student': student})
+
+def student_create(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'students/student_form.html', {'form': form})
+
+def student_update(request, pk):
+    student = Student.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_detail', pk=student.pk)
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/student_form.html', {'form': form})
+
+def student_delete(request, pk):
+    student = Student.objects.get(pk=pk)
+    student.delete()
+    return redirect('student_list')
